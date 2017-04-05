d335a9e32174c35efcaba6ab85bd82740eb374f5
Elioty <roadkiller.cl@gmail.com>
2017-04-05 22:12:18 +0200

    improved example code
    * Removed blank characters
    * Removed useless string buffer use
    * Make use of the task argument forwarding

--- example.cpp
+++ example.cpp
@@ -1,36 +1,33 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
-#include <sstream>
 
 #include "ThreadPool.hpp"
 
 int main()
 {
-    
+
     ThreadPool pool;
     std::vector< std::future<int> > results;
-    std::ostringstream buf;
 
     std::cout << "Workers: " << pool.size() << std::endl;
 
     for(int i = 0; i < 8; ++i) {
         results.emplace_back(
-            pool.enqueue([i] {
+            pool.enqueue([](int i) {
                 std::cout << "hello " << i << std::endl;
                 std::this_thread::sleep_for(std::chrono::seconds(1));
                 std::cout << "world " << i << std::endl;
                 return i*i;
-            })
+            }, i)
         );
     }
 
-    buf << '<' << pool.pending() << '>';
-    std::cout << buf.str();
+    std::cout << "Pending tasks: " << pool.pending() << std::endl;
 
     for(auto && result: results)
         std::cout << result.get() << ' ';
     std::cout << std::endl;
-    
+
     return 0;
 }
